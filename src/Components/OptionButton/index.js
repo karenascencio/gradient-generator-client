5228e980e03c419647aac10bb24afe48b1eaff5d
Karen Ascencio <ascencio.karen@yahoo.com.mx>
2021-10-24 23:19:47 -0500

    radial direction done

--- src/Components/OptionButton/index.js
+++ src/Components/OptionButton/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import './style.scss'
 
 export default function OptionButton (props) {
-  const { buttonText, isActive, buttonHandler, buttonId } = props
+  const { buttonText, isActive, buttonHandler, buttonId, directionName } = props
   return (
     <button
       id={buttonId}
       className={`main__button ${isActive}`}
       onClick={buttonHandler}
+      name={directionName}
     >
       {buttonText}
     </button>
